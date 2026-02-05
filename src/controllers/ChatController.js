8777da7a6701f17611ea91eb5632a5418d406bf9
NayenRC <nay.roman@duocuc.cl>
2026-02-05 00:08:08 -0300

    chat controller

--- src/controllers/ChatController.js
+++ src/controllers/ChatController.js
@@ -15,13 +15,14 @@ export async function sendMessage(req, res) {
   }
 }
 
+// controllers/chatController.js
 export async function getHistory(req, res) {
   try {
     const user_id = req.user.user_id;
     const history = await chatService.getChatHistory(user_id);
-
     res.json(history);
-  } catch (error) {
-    res.status(500).json({ message: 'Error obteniendo historial' });
+  } catch (e) {
+    res.status(500).json({ message: 'Error al obtener historial' });
   }
 }
+
